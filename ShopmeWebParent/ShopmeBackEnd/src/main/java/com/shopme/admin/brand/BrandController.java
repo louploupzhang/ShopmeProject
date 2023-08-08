2eacb978ed774360cc6eb81fe6d7de4d5bad196a
louploupzhang <zhang.sy@hotmail.com>
2023-08-08 18:05:29 -0400

    Brands listing

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandController.java
@@ -0,0 +1,24 @@
+package com.shopme.admin.brand;
+
+import com.shopme.common.entity.Brand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class BrandController {
+    @Autowired
+    private BrandService service;
+
+    @GetMapping("/brands")
+    public String listAll(Model model) {
+        List<Brand> listBrands = service.listAll();
+        model.addAttribute("listBrands", listBrands);
+
+        return "brands/brands";
+    }
+}

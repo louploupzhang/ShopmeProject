1e4282f30eb78a8623f7c3f1bd3c7f1766e831f1
louploupzhang <zhang.sy@hotmail.com>
2023-08-08 22:28:57 -0400

    Brand form

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandController.java
@@ -1,8 +1,9 @@
 package com.shopme.admin.brand;
 
+import com.shopme.admin.category.CategoryService;
 import com.shopme.common.entity.Brand;
+import com.shopme.common.entity.Category;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,13 +13,27 @@ import java.util.List;
 @Controller
 public class BrandController {
     @Autowired
-    private BrandService service;
+    private BrandService brandService;
+
+    @Autowired
+    private CategoryService categoryService;
 
     @GetMapping("/brands")
     public String listAll(Model model) {
-        List<Brand> listBrands = service.listAll();
+        List<Brand> listBrands = brandService.listAll();
         model.addAttribute("listBrands", listBrands);
 
         return "brands/brands";
+    }
+
+    @GetMapping("/brands/new")
+    public String newBrand(Model model) {
+        List<Category> listCategories = categoryService.listCategoriesUsedInForm();
+
+        model.addAttribute("listCategories", listCategories);
+        model.addAttribute("brand", new Brand());
+        model.addAttribute("pageTitle", "Create New Brand");
+
+        return "brands/brand_form";
     }
 }

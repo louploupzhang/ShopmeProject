2c14892f2d0f6d26faa6fe04cdaf0df2df8e4e2e
louploupzhang <zhang.sy@hotmail.com>
2023-08-11 11:44:12 -0400

    Pagination, sort, filter & responsive display for brands

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandRepository.java
@@ -1,6 +1,9 @@
 package com.shopme.admin.brand;
 
 import com.shopme.common.entity.Brand;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface BrandRepository extends PagingAndSortingRepository<Brand, Integer> {
@@ -8,4 +11,7 @@ public interface BrandRepository extends PagingAndSortingRepository<Brand, Integ
     public Long countById(Integer id);
 
     public Brand findByName(String name);
+
+    @Query("select b from Brand b where b.name like %?1%")
+    public Page<Brand> findAll(String keyword, Pageable pageable);
 }

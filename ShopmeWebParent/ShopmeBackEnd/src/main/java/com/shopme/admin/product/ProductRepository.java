9d21b671bbf36dcd98632a42e8592e53d7369e75
louploupzhang <zhang.sy@hotmail.com>
2023-08-11 17:35:47 -0400

    Unit tests for products

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductRepository.java
@@ -3,5 +3,6 @@ package com.shopme.admin.product;
 import com.shopme.common.entity.Product;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+
 public interface ProductRepository extends PagingAndSortingRepository<Product, Integer> {
 }

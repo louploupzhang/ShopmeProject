f5220e5c764bf09aea5b275f04a45d57e7349b8a
louploupzhang <zhang.sy@hotmail.com>
2023-08-12 17:31:25 -0400

    Add/delete extra images on product form

--- ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/common_form.js
+++ ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/common_form.js
@@ -4,15 +4,10 @@ $(document).ready(function () {
     });
 
     $("#fileImage").change(function () {
-        fileSize = this.files[0].size;
-        if (fileSize > MAX_FILE_SIZE) {
-            // this.setCustomValidity("You must choose an image less than 1MB!");
-            this.setCustomValidity("You must choose an image less than " + MAX_FILE_SIZE / 1024 + " KB!");
-            this.reportValidity();
-        } else {
-            this.setCustomValidity("");
-            showImageThumbnail(this);
+        if (!checkFileSize(this)) {
+            return;
         }
+        showImageThumbnail(this);
     });
 });
 
@@ -24,6 +19,18 @@ function showImageThumbnail(fileInput) {
     };
 
     reader.readAsDataURL(file);
+}
+
+function checkFileSize(fileInput) {
+    fileSize = fileInput.files[0].size;
+    if (fileSize > MAX_FILE_SIZE) {
+        fileInput.setCustomValidity("You must choose an image less than " + MAX_FILE_SIZE / 1024 + " KB!");
+        fileInput.reportValidity();
+        return false;
+    } else {
+        fileInput.setCustomValidity("");
+        return true;
+    }
 }
 
 function showModalDialog(title, message) {

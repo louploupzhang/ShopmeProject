12f37872c9f51ad6c423dc40e66e11b7b8a8a807
louploupzhang <zhang.sy@hotmail.com>
2023-08-13 00:56:03 -0400

    Add/delete details on product form

--- ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/product_form_images.js
+++ ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/product_form_images.js
@@ -1,20 +1,12 @@
 var extraImagesCount = 0;
-dropdownBrands = $("#brand");
-dropdownCategories = $("#category");
 
 $(document).ready(function () {
-    $("#shortDescription").richText();
-    $("#fullDescription").richText();
-
-    dropdownBrands.change(function () {
-        dropdownCategories.empty();
-        getCategories();
-    })
-    getCategories();
-
     $("input[name='extraImage']").each(function (index) {
         extraImagesCount++;
         $(this).change(function () {
+            if (!checkFileSize(this)) {
+                return;
+            }
             showExtraImageThumbnail(this, index);
         })
     });
@@ -65,35 +57,3 @@ function removeExtraImage(index) {
     $("#divExtraImage" + index).remove();
 }
 
-function getCategories() {
-    brandId = dropdownBrands.val();
-    url = brandModuleURL + "/" + brandId + "/categories";
-
-    $.get(url, function (responseJson) {
-        $.each(responseJson, function (index, category) {
-            $("<option>").val(category.id).text(category.name).appendTo(dropdownCategories);
-        })
-    })
-}
-
-function checkUnique(form) {
-    productId = $("#id").val();
-    productName = $("#name").val();
-    csrfValue = $("input[name='_csrf']").val();
-
-    params = {id: productId, name: productName, _csrf: csrfValue};
-
-    $.post(checkUniqueUrl, params, function (response) {
-        if (response == "OK") {
-            form.submit();
-        } else if (response == "Duplicate") {
-            showWarningModal("There is another product having same name " + productName);
-        } else {
-            showErrorModal("Unknown response from server")
-        }
-    }).fail(function () {
-        showErrorModal("Could not connect to the server");
-    })
-
-    return false;
-}

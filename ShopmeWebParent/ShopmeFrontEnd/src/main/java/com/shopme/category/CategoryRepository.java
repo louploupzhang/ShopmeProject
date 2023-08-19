c729313181429d9ce7109ce0f7a223779e9ff4fa
louploupzhang <zhang.sy@hotmail.com>
2023-08-18 22:26:36 -0400

    Listing categories in home page

---
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/category/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.shopme.category;
+
+import com.shopme.common.entity.Category;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface CategoryRepository extends CrudRepository<Category, Integer> {
+    @Query("select c from Category c where c.enabled = true order by c.name asc")
+    public List<Category> findAllEnabled();
+}

887b2c56dd287f115909d8bf4364a60e77fcca51
louploupzhang <zhang.sy@hotmail.com>
2023-09-05 23:41:27 -0400

    Change shipping address from checkout page (redirections)

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/address/AddressController.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/address/AddressController.java
@@ -66,9 +66,16 @@ public class AddressController {
         address.setCustomer(customer);
         addressService.save(address);
 
+        String redirectOption = request.getParameter("redirect");
+        String redirectURL = "redirect:/address_book";
+
+        if ("checkout".equals(redirectOption)) {
+            redirectURL += "?redirect=checkout";
+        }
+
         ra.addFlashAttribute("message", "The address has been saved successfully.");
 
-        return "redirect:/address_book";
+        return redirectURL;
     }
 
     @GetMapping("/address_book/edit/{id}")
@@ -104,6 +111,8 @@ public class AddressController {
 
         if ("cart".equals(redirectOption)) {
             redirectURL = "redirect:/cart";
+        } else if ("checkout".equals(redirectOption)) {
+            redirectURL = "redirect:/checkout";
         }
 
         return redirectURL;

df40f58a7fec2eca29ce4fbd6a0c3bc9e4d96383
louploupzhang <zhang.sy@hotmail.com>
2023-09-18 00:35:56 -0400

    Amazon S3 utility class

---
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/AmazonS3UtilTests.java
@@ -0,0 +1,41 @@
+package com.shopme.admin;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.List;
+
+public class AmazonS3UtilTests {
+
+    @Test
+    public void testListFolder() {
+        String folderName = "test-upload";
+        List<String> listKeys = AmazonS3Util.listFolder(folderName);
+        listKeys.forEach(System.out::println);
+    }
+
+    @Test
+    public void testUploadFile() throws FileNotFoundException {
+        String folderName = "test-upload";
+        String fileName = "086_Spuydjeks_022.jpg";
+        String filePath = "E:\\Etudes\\Drawing\\临摹素材\\furry\\spuydjeks\\[e621] Spuydjeks\\" + fileName;
+
+        InputStream inputStream = new FileInputStream(filePath);
+
+        AmazonS3Util.uploadFile(folderName, fileName, inputStream);
+    }
+
+    @Test
+    public void testDeleteFile() {
+        String fileName = "test-upload/056_Spuydjeks_052.jpg";
+        AmazonS3Util.deleteFile(fileName);
+    }
+
+    @Test
+    public void testRemoveFolder() {
+        String folderName = "test-upload";
+        AmazonS3Util.removeFolder(folderName);
+    }
+}

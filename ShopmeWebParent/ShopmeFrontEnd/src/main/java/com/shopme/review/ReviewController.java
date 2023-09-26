7d58865e96e9993bbae5a90a3396cb6b750663c2
louploupzhang <zhang.sy@hotmail.com>
2023-09-26 18:31:15 -0400

    Review listing of a product

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/review/ReviewController.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/review/ReviewController.java
@@ -3,8 +3,11 @@ package com.shopme.review;
 import com.shopme.Utility;
 import com.shopme.common.entity.Customer;
 import com.shopme.common.entity.Review;
+import com.shopme.common.entity.product.Product;
+import com.shopme.common.exception.ProductNotFoundException;
 import com.shopme.common.exception.ReviewNotFoundException;
 import com.shopme.customer.CustomerService;
+import com.shopme.product.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
@@ -24,6 +27,8 @@ public class ReviewController {
     private ReviewService reviewService;
     @Autowired
     private CustomerService customerService;
+    @Autowired
+    private ProductService productService;
 
     @GetMapping("/reviews")
     public String listFirstPage(Model model) {
@@ -77,5 +82,49 @@ public class ReviewController {
             ra.addFlashAttribute("message", e.getMessage());
             return defaultRedirectURL;
         }
+    }
+
+    @GetMapping("/rating/{productAlias}/page/{pageNum}")
+    public String listByProductByPage(Model model,
+                                      @PathVariable(name = "productAlias") String productAlias,
+                                      @PathVariable(name = "pageNum") int pageNum,
+                                      String sortField, String sortDir) {
+        Product product = null;
+        try {
+            product = productService.getProduct(productAlias);
+        } catch (ProductNotFoundException e) {
+            return "error/404";
+        }
+
+        Page<Review> page = reviewService.listByProduct(product, pageNum, sortField, sortDir);
+        List<Review> listReviews = page.getContent();
+
+        model.addAttribute("totalPages", page.getTotalPages());
+        model.addAttribute("totalItems", page.getTotalElements());
+        model.addAttribute("currentPage", pageNum);
+        model.addAttribute("sortField", sortField);
+        model.addAttribute("sortDir", sortDir);
+        model.addAttribute("reverseSortDir", sortDir.equals("asc") ? "desc" : "asc");
+        model.addAttribute("listReviews", listReviews);
+        model.addAttribute("product", product);
+
+        long startCount = (pageNum - 1) * ReviewService.REVIEWS_PER_PAGE + 1;
+        model.addAttribute("startCount", startCount);
+
+        long endCount = startCount + ReviewService.REVIEWS_PER_PAGE - 1;
+        if (endCount > page.getTotalElements()) {
+            endCount = page.getTotalElements();
+        }
+
+        model.addAttribute("endCount", endCount);
+        model.addAttribute("pageTitle", "Reviews for " + product.getShortName());
+
+        return "reviews/reviews_product";
+    }
+
+    @GetMapping("/ratings/{productAlias}")
+    public String listByProductFirstPage(@PathVariable(name = "productAlias") String productAlias,
+                                         Model model) {
+        return listByProductByPage(model, productAlias, 1, "reviewTime", "desc");
     }
 }
